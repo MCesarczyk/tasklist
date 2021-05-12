38adfb5e3d0b9919f7cbdab644e316f82961f34d
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-12 16:05:12 +0200

    adding task remove functionality

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -25,22 +38,21 @@
                 `;
 
             document.querySelector(".js-tasks").innerHTML = htmlString;
+
+            const removeButtons = document.querySelectorAll(".js-remove");
+
+            removeButtons.forEach((removeButton, index) => {
+                removeButton.addEventListener("click", () => {
+                    removeTask(index);
+                });
+            });
         }
-    };
-
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
-        render();
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-taskInput").value.trim();
-        console.log(newTaskContent);
 
         if (newTaskContent === "") {
             return;
