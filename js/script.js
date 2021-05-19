1242428191895246db3a8d383faefc592e30915e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-19 20:02:41 +0200

    task unchecking added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
     const addNewTask = (newTaskContent) => {
         tasks = [
             ...tasks,
-            { content: newTaskContent }
+            { content: newTaskContent, done: false }
         ];
 
         render();
@@ -24,14 +24,21 @@
         render();
     }
 
-    const toggleTaskDone = (index) => {
+    const changeTaskStatus = (index, taskStatus) => {
         tasks = [
             ...tasks.slice(0, index),
-            { ...tasks[index], done: true },
+            { ...tasks[index], done: taskStatus },
             ...tasks.slice(index + 1),
         ]
 
         render();
+    }
+
+    const toggleTaskDone = (index) => {
+        let taskStatus = tasks[index].done;
+        taskStatus = (taskStatus === true) ? false : true;
+
+        changeTaskStatus(index, taskStatus);
     }
 
     const bindEvents = () => {
